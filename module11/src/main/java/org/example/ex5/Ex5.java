68f3821d36180d631206911818a095828e401649
Maryna Piatnytsia <mpatnica924@gmail.com>
2022-10-06 22:16:29 +0300

    zip method modified

--- module11/src/main/java/org/example/ex5/Ex5.java
+++ module11/src/main/java/org/example/ex5/Ex5.java
@@ -1,11 +1,15 @@
 package org.example.ex5;
 
+import java.util.Iterator;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class Ex5 {
 
   public static <T> Stream<T> zip(Stream<T> first, Stream<T> second) {
-    return Stream.concat(first, second).limit(Math.min(first.count(), second.count()));
-  }
-
+    Iterator<T> firstIterator = first.iterator();
+    Iterator<T> secondIterator = second.iterator();
+    return IntStream.iterate(0, x -> firstIterator.hasNext() && secondIterator.hasNext(), x -> ++x)
+        .mapToObj(x -> x % 2 == 0 ? firstIterator.next() : secondIterator.next());
+    }
 }
